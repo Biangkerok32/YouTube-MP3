09ada14ab3402dbd2f420bcad46ddea63de9cbc0
Danie Britto Kuthalane <dannybritto96@gmail.com>
2018-04-02 11:13:19 +0530

    Update flask_app.py
    Returns Video title which is displayed in the App UI

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python
 from __future__ import unicode_literals
 from flask import Flask,request
 import youtube_dl
+import os
 app = Flask(__name__)
 
 @app.route("/hello",methods=['GET','POST'])
@@ -20,10 +22,12 @@ def hello():
                     'preferredquality':'160',
                     }],
                 }
+        info_dict = None
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url,download=False)
             ydl.download([url])
         print(url)
-    return "Hello World!"
+    return info_dict.get('title',None)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
